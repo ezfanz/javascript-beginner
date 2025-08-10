651aaf19c9949bc7b2acf9efac9ae09f086b7a77
ezfanz <ezfanz1996@gmail.com>
2025-08-10 23:09:42 +0800

    feat : Added new tutorial
    1. String Method
    2. Switch

---
+++ js/stringMethods.js
@@ -0,0 +1,39 @@
+/* string methods = allow you to manipulate and work
+with text (strings) */
+
+let userName = "BroCode";
+
+/* Find Character  */
+userName.charAt(1);
+
+/* Find Index*/
+userName.indexOf("o");
+
+/* Find Last Of Index */
+userName.lastIndexOf("o");
+
+/* Find Length Of The String */
+userName.length;
+
+/* trim */
+userName.trim();
+
+/* UpperCase & LowerCase */
+userName.toUpperCase();
+userName.toLowerCase();
+
+/*repeat & startWith/endsWith */
+userName.repeat(3);
+let result = userName.startsWith(" ");
+let result2 = userName.endsWith(" ");
+
+/*includes*/
+let result3 = userName.includes(" ");
+
+if(result3){
+    console.log("Your username can't begin with ' '");
+}else{
+    console.log(userName);
+}
+
+// console.log(userName.repeat(3));
--- js/switch.js
+++ js/switch.js
@@ -1,27 +1,51 @@
 /*SWITCH = can be an efficient replacement to many else if statements */
 
-let day = 1;
+// let day = 'pizza';
 
-switch(day){
-    case 1:
-        console.log("It is Monday");
+// switch(day){
+//     case 1:
+//         console.log("It is Monday");
+//         break;
+//     case 2:
+//         console.log("It is Tuesday");
+//         break;
+//     case 3:
+//         console.log("It is Wednesday");
+//         break;
+//     case 4:
+//         console.log("It is Thursday");
+//         break;
+//     case 5:
+//         console.log("It is Friday");
+//         break;
+//     case 6:
+//         console.log("It is Saturday");
+//         break;
+//     case 7:
+//         console.log("It is Sunday");
+//         break;
+//     default:
+//         console.log(`${day} is not a day`);
+// }
+
+let testScore = 92;
+let letterGrade;
+
+switch(true){
+    case testScore >= 90:
+        letterGrade = "A";
         break;
-    case 2:
-        console.log("It is Tuesday");
+    case testScore >= 80:
+        letterGrade = "B";
         break;
-    case 3:
-        console.log("It is Wednesday");
+    case testScore >= 70:
+        letterGrade = "C";
         break;
-    case 4:
-        console.log("It is Thursday");
+    case testScore >= 60:
+        letterGrade = "D";
         break;
-    case 5:
-        console.log("It is Friday");
-        break;
-    case 6:
-        console.log("It is Saturday");
-        break;
-    case 7:
-        console.log("It is Sunday");
-        break;
+    default:
+        letterGrade = "F";
 }
+
+console.log(letterGrade);
