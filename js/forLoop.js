c498d250bf210f5148c9639bc4b2c7c5ed8f121f
ezfanz <ezfanz1996@gmail.com>
2025-08-25 15:33:25 +0800

    feat : added new tutorial

---
+++ js/forLoop.js
@@ -0,0 +1,17 @@
+/* For Loop = repeat some code a LIMITED amount of times */
+
+// for(let i = 10; i > 0; i--){
+//     console.log(i);
+
+// }
+
+// console.log("HAPPY NEW YEAR!")
+
+for (let i = 1; i <= 20; i++) {
+  if (i == 13) {
+    // continue;
+    break;
+  } else {
+  }
+  console.log(i);
+}
